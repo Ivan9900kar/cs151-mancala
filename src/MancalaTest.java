87c768dad1f9f096c0bb9f49cd6736dcdb34f595
IsaMak <isaiah.mak@sjsu.edu>
2026-04-13 17:57:26 -0700

    Forgot to include in push

---
+++ src/MancalaTest.java
@@ -0,0 +1,7 @@
+public class MancalaTest {
+    public static void main(String[] args) {
+        StoneContainer s = new StoneContainer(3);
+        System.out.println(s.takeStones());
+        System.out.println(s.getStones());
+    }
+}
