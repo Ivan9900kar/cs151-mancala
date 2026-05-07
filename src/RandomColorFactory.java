de5a64c3efa62923ca3b6b88c8ba1b6e0ab28190
IsaMak <isaiah.mak@sjsu.edu>
2026-05-06 20:41:13 -0700

    javadoc for RandomColorFactory

--- src/RandomColorFactory.java
+++ src/RandomColorFactory.java
@@ -1,17 +1,36 @@
 import java.awt.*;
 
+/**
+ * RandomColorFactory class supplies methods to draw/fill shapes with color
+ */
 public class RandomColorFactory {
+    /**
+     * Draws the shape filled in with the color Gray
+     * @param g2
+     * @param shape Shape to be drawn
+     */
     public static void drawGray(Graphics2D g2, Shape shape) {
         g2.setColor(Color.GRAY);
         g2.fill(shape);
         g2.setColor(Color.BLACK);
     }
+
+    /**
+     * Draws the shape filled with a random color and black border
+     * @param g2
+     * @param shape
+     */
     public static void drawRandomColor(Graphics2D g2, Shape shape) {
          g2.setColor(new Color(randomRGB(), randomRGB(), randomRGB()));
          g2.fill(shape);
          g2.setColor(Color.BLACK);
          g2.draw(shape);
     }
+
+    /**
+     * Random RGB value ranging from 100 to 249
+     * @return
+     */
     public static int randomRGB() {
         return (int) (Math.random() * 150) + 100;
     }
