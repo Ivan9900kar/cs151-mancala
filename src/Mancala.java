7625aedac7aa7d79d9450725ec32c0c6d6cdc247
Ivan9900kar <syrologoshlo@gmail.com>
2026-05-06 20:25:14 -0700

    Pit and Mancala comments

--- src/Mancala.java
+++ src/Mancala.java
@@ -1,25 +1,48 @@
 import java.awt.*;
 
 /**
- * Class representing the Mancala for each player. Holds the stones that its player has captured.
+ * Class representing the Mancala for each player.
+ * Holds the stones that are currently in the mancala (from StoneContainer) and its position on the board.
  */
 public class Mancala extends StoneContainer {
     BoardStyle strategy;
     private int row; 
 
+    /**
+     * Mancala default constructor.
+     */
     public Mancala() {
         super();
     }
+
+    /**
+     * Mancala constructor with specified starting amount of stones.
+     * @param stones the number of stones to start out with
+     */
     public Mancala(int stones) {
         super(stones);
     }
 
+    /**
+     * Sets the row of the mancala.
+     * @param row the row of the mancala (0 for player A, 1 for player B)
+     */
     public void setRow(int row) { 
         this.row = row;
     }
+
+    /**
+     * Sets the strategy for drawing the mancala.
+     * @param strategy the strategy to draw with
+     */
     public void setStrategy(BoardStyle strategy) {
         this.strategy = strategy;
     }
+
+    /**
+     * Draws the mancala using the strategy pattern.
+     * @param g2 the relevant Graphics2D object
+     */
     public void draw(Graphics2D g2) {
         // draw mancala using strategy pattern
         strategy.drawMancala(g2, getX(), getY(), row);
--- src/Pit.java
+++ src/Pit.java
@@ -1,7 +1,8 @@
 import java.awt.*;
 
 /**
- * Class representing a pit on the board. Holds the stones that are currently in the pit.
+ * Class representing a pit on the board.
+ * Holds the stones that are currently in the pit (from StoneContainer), and its position on the board.
  */
 public class Pit extends StoneContainer {
     BoardStyle strategy;
@@ -12,8 +13,9 @@ public class Pit extends StoneContainer {
      * Default constructor of a pit.
      */
     public Pit() {
-        this(0);
+        super();
     }
+
     /**
      * Constructs a pit that will start with a specified amount of stones inside.
      * @param stones the stones that the pit will start out with
@@ -23,21 +25,47 @@ public class Pit extends StoneContainer {
         this.row = -1;
         this.col = -1;
     }
+
+    /**
+     * Gets the row of the pit.
+     * @return the row of the pit (0 for player A, 1 for player B)
+     */
     public int getRow() {
         return this.row;
     }
+
+    /**
+     * Sets the row of the pit.
+     * @param row the specified row of the pit (0 for player A, 1 for player B)
+     */
     public void setRow(int row) {
         this.row = row;
     }
+
+    /**
+     * Returns the column of the pit.
+     * @return the column of the pit
+     */
     public int getCol() {
         return this.col;
     }
+
+    /**
+     * Sets the column of the pit.
+     * @param col the specified column of the pit
+     */
     public void setCol(int col) {
         this.col = col;
     }
-        public void setStrategy(BoardStyle strategy) {
+
+    /**
+     * Sets the strategy for drawing the pit.
+     * @param strategy the strategy to draw with
+     */
+    public void setStrategy(BoardStyle strategy) {
         this.strategy = strategy;
     }
+
     /**
      * Takes all of the stones out of the pit and returns the taken amount.
      * @return the number of stones that were inside of the pit
@@ -47,11 +75,21 @@ public class Pit extends StoneContainer {
         setStones(0);
         return ret;
     }
+
+    /**
+     * Draws the pit using the strategy pattern.
+     * @param g2 the relevant Graphics2D object
+     */
     public void draw(Graphics2D g2) {
         // draw pit using strategy pattern
         strategy.drawPit(g2, getX(), getY(), row, col);
     }
 
+    /**
+     * Checks if a given point (from a mouse click) is within the bounds of a pit.
+     * @param p the point to check
+     * @return true if the point is inside the pit, false otherwise
+     */
     public boolean contains(Point p) {
         double x = getX();
         double y = getY();
