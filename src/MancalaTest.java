a11a15b051f53d75c7fea81e1589a2450a1f58f7
Ivan9900kar <syrologoshlo@gmail.com>
2026-05-06 22:50:36 -0700

    some mancalatest comments

--- src/MancalaTest.java
+++ src/MancalaTest.java
@@ -2,28 +2,36 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MancalaTest {
-    private static GameView ref;
     public static void main(String[] args) {
         play();
     }
+    /**
+     * The method to play the game.
+     * Creates the model, view, and controller, and displays the game window.
+     */
     public static void play() {
+        // model
         GameModel model = new GameModel(6);
 
+        // game window
         JFrame frame = new JFrame();
         frame.setSize(1500, 800);
 
+        // view
         GameView view = model.getCurrentView();
         frame.add(view, BorderLayout.CENTER);
-        ref = view;
 
+        // style setup
         view.styleMenu();
 
+        // number of stones setup
         int numStones = view.stonesMenu();
         model.setStartingStones(numStones);
 
+        // controller
         new GameController(model, view);
 
-        //frame.pack();
+        // finish frame setup
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
