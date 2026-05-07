fa5b87c4622c5c422354a517e79aac3ffaf52cae
Ivan9900kar <syrologoshlo@gmail.com>
2026-05-06 19:35:57 -0700

    gamecontroller comments

--- src/GameController.java
+++ src/GameController.java
@@ -7,6 +7,11 @@ import java.awt.event.MouseEvent;
 public class GameController {
     private final GameModel model;
 
+    /**
+     * Constructor for GameController. Adds action listeners to the confirm and undo buttons in the view and a mouse listener for the game board.
+     * @param model the reference to the GameModel object
+     * @param view the reference to the GameView object
+     */
     public GameController(GameModel model, GameView view) {
         this.model = model;
         view.addConfirmActionListener(e -> model.confirm());
@@ -15,6 +20,10 @@ public class GameController {
         view.update();
     }
 
+    /**
+     * Inner class for handling mouse clicks on the game board (specifically the pits) using MouseAdapter.
+     * Detects which pit was clicked on and calls the move method in the model with the pit's row and column.
+     */
     private class MyMouseListener extends MouseAdapter {
         @Override
         public void mouseClicked(MouseEvent event) {
